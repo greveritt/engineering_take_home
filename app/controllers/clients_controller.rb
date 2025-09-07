bc0796a7213c93cc6f7a26222a4f55013c25cd10
Gregory R. Everitt <me@gregeveritt.com>
2025-09-07 22:33:58 +0000

    Fixes missing routes in clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: %i[ show update destroy ]
+  before_action :set_client, only: %i[ show ]
 
   # GET /clients
   # GET /clients.json
